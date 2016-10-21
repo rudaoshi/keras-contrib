42821f3f0fcd0373c683b9b1e8e6e6afa187f41f
rudaoshi <sunmingming@gmail.com>
2016-10-21 16:21:56 +0800

    reorganize

--- neural_machine/tasks/language/common/corpus/sequence_pair_corpus.py
+++ neural_machine/tasks/language/common/corpus/sequence_pair_corpus.py
@@ -2,7 +2,7 @@ __author__ = 'Sun'
 
 import codecs
 import numpy as np
-
+import json, logging
 import itertools
 from collections import Counter
 from neural_machine.tasks.language.common.corpus.sequence_corpus import SequenceCorpus
@@ -12,10 +12,12 @@ class SequencePairCorpus(object):
     def __init__(self,
                  source_with_start=False, source_with_end = False, source_with_unk = False,
                  target_with_start=False, target_with_end=False, target_with_unk=False,
+                 same_length = False
                  ):
 
         self.source_corpus = SequenceCorpus(source_with_start, source_with_end, source_with_unk)
         self.target_corpus = SequenceCorpus(target_with_start, target_with_end, target_with_unk)
+        self.same_length = same_length
 
         self.corpus = []
 
@@ -27,6 +29,9 @@ class SequencePairCorpus(object):
                 continue
 
             src_seq, tgt_seq = line.split('\t')
+            if self.same_length and len(src_seq) != len(tgt_seq):
+                logging.error("src and tgt seq not in same length {0} {1} {2}".format(len(src_seq), len(tgt_seq), json.dumps(line)))
+                continue
 
             self.source_corpus.update(src_seq, source_segmenter)
             self.target_corpus.update(tgt_seq, target_segmenter)
